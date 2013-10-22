108c98d231e60e360cd576a99446d9728689ad2f
DaltonLacy <dlacy2016@gmail.com>
2013-10-22 08:06:51 -0400

    Problem 3

---
+++ MoreChapterProblems/MoreChapterProblems/Furniture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoreChapterProblems
+{
+    class Furniture
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Type P for Pine, O for Oak, M for Mahogany");
+            input = Console.ReadLine();
+
+            if (input == "P")
+            {
+                Console.WriteLine("Price is $100");
+            }
+
+            if (input != "P")
+            {
+                Console.WriteLine("Price is $0");
+            }
+
+            if (input == "O")
+            {
+                Console.WriteLine("Price is $225");
+            }
+
+            if (input != "O")
+            {
+                Console.WriteLine("Price is $0");
+            }
+
+            if (input == "M")
+            {
+                Console.WriteLine("Price is $310");
+            }
+
+            if (input != "M")
+            {
+                Console.WriteLine("Price is $0");
+            }
+
+          
+        }
+    }
+}
