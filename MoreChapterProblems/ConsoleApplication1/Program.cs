58fb4303a47c8615a8b62104279e57f9ff87daf3
DaltonLacy <dlacy2016@gmail.com>
2013-10-22 08:38:39 -0400

    Problem 2

---
+++ MoreChapterProblems/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zip = new int[10];
+            zip[0] = 18042;
+            zip[1] = 18043;
+            zip[2] = 18044;
+            zip[3] = 18045;
+            zip[4] = 18046;
+            zip[5] = 18047;
+            zip[6] = 18048;
+            zip[7] = 18049;
+            zip[8] = 18031;
+            zip[9] = 18032;
+             
+            Console.WriteLine("What is your zip code?");
+            string input = Console.ReadLine();
+        }
+    }
+}
