b58b729c44dccd99af3c66813adf049cb90d95fb
DaltonLacy <dlacy2016@gmail.com>
2013-10-23 08:27:29 -0400

    Monogram started

---
+++ MoreChapterProblems/Pg244Problem3/Monogram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pg244Problem3
+{
+    class Monogram
+    {
+        static void Main(string[] args)
+        {
+            string myfirst;
+            string mymiddle;
+            string mylast;
+
+            string friendfirst;
+            string friendmiddle;
+            string friendlast;
+            
+
+            DisplayMonogram(myfirst, mymiddle, mylast);
+            
+        }
+        public static void DisplayMonogram(string first, string middle, string last)
+        {
+            Console.WriteLine("** {0}. {1}. {2}. **", first, middle, last);
+
+
+        }
+        }
+    }
+}
