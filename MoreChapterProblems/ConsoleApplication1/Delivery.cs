1bad562d84e9c7fd3b16291da0dd11586d206023
DaltonLacy <dlacy2016@gmail.com>
2013-10-23 08:19:38 -0400

    2 + 4 done

--- MoreChapterProblems/ConsoleApplication1/Delivery.cs
+++ MoreChapterProblems/ConsoleApplication1/Delivery.cs
@@ -5,10 +5,11 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class Delivery
     {
         static void Main(string[] args)
         {
+            bool found = false;
             int[] zip = new int[10];
             zip[0] = 18042;
             zip[1] = 18043;
@@ -20,9 +21,26 @@ namespace ConsoleApplication1
             zip[7] = 18049;
             zip[8] = 18031;
             zip[9] = 18032;
-             
+
+
+
             Console.WriteLine("What is your zip code?");
             string input = Console.ReadLine();
+            int myzip = Convert.ToInt32(input);
+
+            for (int i = 0; i < zip.Length; i++)
+            {
+                
+                if(myzip == zip[i]) 
+                {
+                    Console.WriteLine("Yes we deliever there");
+                }
+                if (!found)
+                {
+                    Console.WriteLine("No we do not deliever there");
+                }
+            }
+
         }
     }
 }
---
+++ MoreChapterProblems/Pg195Problem4/ChatAWhile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pg195Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zip = new int[10];
+          
+            zip[0] = 262;
+            zip[1] = 414;
+            zip[2] = 608;
+            zip[3] = 715;
+            zip[4] = 815;
+            zip[5] = 920;
+
+            Console.WriteLine("Enter your area code");
+            string input = Console.ReadLine();
+            int myzip = Convert.ToInt32(input);
+
+            {
+
+                if (myzip == zip[0])
+                {
+                    Console.WriteLine("Per-Minute Rate($) is 0.07, which is multiplied by {0} minutes of the call");
+                }
+                if (myzip == zip[1])
+                {
+                    Console.WriteLine("Per-Minute Rate($) is 0.10, which is multiplied by {0} minutes of the call");
+                }
+                if (myzip == zip[2])
+                {
+                    Console.WriteLine("Per-Minute Rate($) is 0.05, which is multiplied by {0} minutes of the call");
+                }
+                if (myzip == zip[3])
+                {
+                    Console.WriteLine("Per-Minute Rate($) is 0.16, which is multiplied by {0} minutes of the call");
+                }
+                if (myzip == zip[4])
+                {
+                    Console.WriteLine("Per-Minute Rate($) is 0.24, which is multiplied by {0} minutes of the call");
+                }
+                if (myzip == zip[5])
+                {
+                    Console.WriteLine("Per-Minute Rate($) is 0.14, which is multiplied by {0} minutes of the call");
+                }
+               
+            }
+        }
+    }
+}
